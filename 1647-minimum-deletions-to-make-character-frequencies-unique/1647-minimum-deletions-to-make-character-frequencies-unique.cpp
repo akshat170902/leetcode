cb4c9655fd4009ed85a975a047176f25625d7527
akshat170902 <95205174+akshat170902@users.noreply.github.com>
2022-06-28 08:46:52 +0530

    Time: 253 ms (5.05%), Space: 18.8 MB (6.86%) - LeetHub

---
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int minDeletions(string s) {
+       unordered_map<char,int>ml;
+        int ans=0;
+        int least=1e9;
+        int large=0;
+        for(int i=0;i<s.size();i++){
+            ml[s[i]]++;
+            large=max(large,ml[s[i]]);
+        }
+        vector<int>prev;
+        unordered_map<int,int>m;
+        for(auto it=ml.begin();it!=ml.end();it++){
+            m[it->second]++;
+        }
+        for(int i=1;i<large+1;i++){
+            if(m.count(i)==0){
+                prev.push_back(i);
+            }
+            else if(m[i]==1){
+                continue;
+            }
+            else{
+                while (m[i]!=1){
+                    int last=0;
+                    if(prev.size()>0){
+                        last=prev.back();
+                        prev.pop_back();
+                    }
+                    ans+=i-last;
+                    m[i]--;
+                }
+            }
+        }
+        return ans;
+    }
+};
