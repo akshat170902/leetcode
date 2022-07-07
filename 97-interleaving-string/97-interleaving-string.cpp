985c1430e74200458f15bbb579c15b5b0abf65bd
akshat170902 <95205174+akshat170902@users.noreply.github.com>
2022-07-07 08:06:07 +0530

    Time: 3 ms (85.12%), Space: 7.1 MB (54.34%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,24 +1,30 @@
 class Solution {
 public:
-    bool rec(string s1,string s2,string s3,int i,int j,int k,vector<vector<int>>&dp){
-        if(k==s3.size()&&i==s1.size()&&j==s2.size())return true;
-        if(i>s1.size()||j>s2.size())return false;
-        if(dp[i][j]!=-1)return dp[i][j];
-        if(s3[k]==s2[j]&&s3[k]==s1[i]){
-            return dp[i][j]= rec(s1,s2,s3,i+1,j,k+1,dp)||rec(s1,s2,s3,i,j+1,k+1,dp);
-        }
-        else if(s1[i]==s3[k]){
-            return dp[i][j]= rec(s1,s2,s3,i+1,j,k+1,dp);
-        }
-        else if(s3[k]==s2[j]){
-            return dp[i][j]= rec(s1,s2,s3,i,j+1,k+1,dp);
-        }
-        else{
-            return dp[i][j]= false;
-        }
-    }
     bool isInterleave(string s1, string s2, string s3) {
-        vector<vector<int>>dp(s1.size()+1,(vector<int>(s2.size()+1,-1)));
-        return rec(s1,s2,s3,0,0,0,dp);
+        vector<vector<int>>dp(s1.size()+1,(vector<int>(s2.size()+1,0)));
+        if(s3.size()!=s1.size()+s2.size()){
+            return false;
+        }
+        for(int i=s1.size();i>=0;i--){
+            for(int j=s2.size();j>=0;j--){
+                int k=i+j;
+                if(i==s1.size()&&j==s2.size()){
+                    dp[i][j]=1;
+                }
+                else if(s3[k]==s2[j]&&s3[k]==s1[i]){
+                    dp[i][j]= dp[i+1][j]||dp[i][j+1];
+                }
+                else if(s1[i]==s3[k]){
+                    dp[i][j]= dp[i+1][j];
+                }
+                else if(s3[k]==s2[j]){
+                    dp[i][j]= dp[i][j+1];
+                }
+                else{
+                    dp[i][j]= false;
+                }  
+            }
+        }
+        return dp[0][0];
     }
 };
