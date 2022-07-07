953ec76975e6c8393265b9ebd20075d663fe8300
akshat170902 <95205174+akshat170902@users.noreply.github.com>
2022-07-07 08:10:52 +0530

    Time: 3 ms (85.12%), Space: 6.3 MB (79.41%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool isInterleave(string s1, string s2, string s3) {
-        vector<vector<int>>dp(s1.size()+1,(vector<int>(s2.size()+1,0)));
+        vector<int>cur(s2.size()+1,0),prev(s2.size()+1,0);
         if(s3.size()!=s1.size()+s2.size()){
             return false;
         }
@@ -9,22 +9,23 @@ public:
             for(int j=s2.size();j>=0;j--){
                 int k=i+j;
                 if(i==s1.size()&&j==s2.size()){
-                    dp[i][j]=1;
+                    cur[j]=1;
                 }
                 else if(s3[k]==s2[j]&&s3[k]==s1[i]){
-                    dp[i][j]= dp[i+1][j]||dp[i][j+1];
+                    cur[j]= prev[j]||cur[j+1];
                 }
                 else if(s1[i]==s3[k]){
-                    dp[i][j]= dp[i+1][j];
+                    cur[j]= prev[j];
                 }
                 else if(s3[k]==s2[j]){
-                    dp[i][j]= dp[i][j+1];
+                    cur[j]= cur[j+1];
                 }
                 else{
-                    dp[i][j]= false;
+                    cur[j]= false;
                 }  
             }
+            prev=cur;
         }
-        return dp[0][0];
+        return cur[0];
     }
 };
