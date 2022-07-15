1848eca842f1165f4938ddec02896bc37bcbbb97
akshat170902 <95205174+akshat170902@users.noreply.github.com>
2022-07-15 08:27:37 +0530

    Time: 338 ms (38.05%), Space: 17.2 MB (37.60%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -1,21 +1,14 @@
 class Solution {
 public:
-    int rec(vector<int>&nums,int idx,vector<int>&dp){
-        if(idx>=nums.size()){
-            return 1e9;
-        }
-        if(idx==nums.size()-1){
-            return 0;
-        }
-        if(dp[idx]!=-1)return dp[idx];
-        int ans=1e9;
-        for(int i=1;i<=nums[idx];i++){
-            ans=min(ans,rec(nums,idx+i,dp)+1);
-        }
-        return dp[idx]=ans;
-    }
     int jump(vector<int>& nums) {
-        vector<int>dp(nums.size(),-1);
-        return rec(nums,0,dp);
+        vector<int>dp(nums.size(),0);
+        for(int idx=nums.size()-2;idx>=0;idx--){
+            int ans=1e9;
+            for(int i=1;i<=nums[idx]&&i+idx<nums.size();i++){
+                ans=min(ans,dp[idx+i]+1);
+            }
+        dp[idx]=ans;
+        }
+        return dp[0];
     }
 };
