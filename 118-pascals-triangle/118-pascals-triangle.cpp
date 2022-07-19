921d5e0ab846b81e6b80ef2dfa09a6613227861e
akshat170902 <95205174+akshat170902@users.noreply.github.com>
2022-07-19 07:17:41 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (71.06%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -4,12 +4,13 @@ public:
         vector<vector<int>>v(numRows);
         int i=0;
         for(int i=0;i<numRows;i++){
-        for(int j=0;j<=i;j++){
-            if(j==0||j==i)v[i].push_back(1);
-            else{
+          for(int j=0;j<=i;j++){
+            if(j==0||j==i)
+                v[i].push_back(1);
+            else 
                 v[i].push_back(v[i-1][j-1]+v[i-1][j]);
             }
-        }}
+        }
         return v;
     }
 };
