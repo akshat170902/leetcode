f4e2c6aa3644b24b6562c12529d22509642d25aa
akshat170902 <95205174+akshat170902@users.noreply.github.com>
2022-07-25 12:36:14 +0530

    Time: 33 ms (22.07%), Space: 19.8 MB (75.82%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int ans=0;
-        vector<int>l(height.size()),r(height.size());
-        l[0]=height[0];
-        r[height.size()-1]=height[height.size()-1];
-        for(int i=1;i<height.size();i++){
-             l[i]=max(l[i-1],height[i]);
-            r[height.size()-i-1]=max(r[height.size()-i],height[height.size()-1-i]);
-            cout<<l[i]<<" "<<r[height.size()-i-1]<<endl;
+        int lo=0,hi=height.size()-1;
+        int res=0;
+        int leftmax=0,rightmax=0;
+        while(lo<hi){
+            if(height[lo]<=height[hi]){
+                leftmax=max(height[lo],leftmax);
+                res+=leftmax-height[lo];
+                lo++;
+            }
+            else{
+                rightmax=max(height[hi],rightmax);
+                res+=rightmax-height[hi];
+                hi--;
+            }
         }
-         for(int i=1;i<height.size()-1;i++){
-             ans+=((min(l[i-1],r[i+1])-height[i])>0?min(l[i-1],r[i+1])-height[i]:0);
-         }
-        return ans;
+        return res;
     }
 };
