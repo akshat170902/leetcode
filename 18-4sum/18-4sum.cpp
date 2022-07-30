031ec140c7bc2680e5008dc313c562afa2e67758
akshat170902 <95205174+akshat170902@users.noreply.github.com>
2022-07-30 16:19:20 +0530

    Time: 170 ms (31.41%), Space: 13.3 MB (54.08%) - LeetHub

--- 18-4sum/18-4sum.cpp
+++ 18-4sum/18-4sum.cpp
@@ -3,26 +3,34 @@ public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         vector<vector<int>>ans;
         sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size();i++){                
-            if(i!=0&&nums[i]==nums[i-1])continue;
-            for(int j=i+1;j<nums.size();j++){
-                if(j!=i+1&&nums[j]==nums[j-1])continue;
-                long long int tar=(long long int)target-nums[i]-nums[j];
-                int right=nums.size()-1,left=j+1;
-                while(left<right){
-                    if(left!=j+1&&nums[left]==nums[left-1])left++;
-                    else if(right!=nums.size()-1&&nums[right]==nums[right+1])right--;
-                    else if(nums[left]+nums[right]<tar){
-                        left++;
-                    }
-                    else if(nums[left]+nums[right]>tar){
-                        right--;
-                    }
-                    else{
-                        // cout<<j<<endl;
-                        vector<int>v={nums[i],nums[j],nums[left],nums[right]};
-                        ans.push_back(v);
-                        left++;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(i==0||nums[i]!=nums[i-1]){
+                for(int j=i+1;j<n;j++){
+                    if(j==i+1||nums[j]!=nums[j-1]){
+                        long long tar=(long long)target-nums[i]-nums[j];
+                        int right=n-1,left=j+1;
+                        while(left<right){
+                            if(left!=j+1&&nums[left]==nums[left-1]){
+                                left++;
+                                continue;
+                            }
+                            if(right!=n-1&&nums[right]==nums[right+1]){
+                                right--;
+                                continue;
+                            }
+                            if(nums[left]+nums[right]==tar){
+                                vector<int>temp={nums[i],nums[j],nums[left],nums[right]};
+                                ans.push_back(temp);
+                                left++;
+                            }
+                            else if(nums[left]+nums[right]<tar){
+                                left++;
+                            }
+                            else{
+                                right--;
+                            }
+                        }
                     }
                 }
             }
