f96fe86937f6c7ea35e6bfb6cbcb0687e466f86d
akshat170902 <95205174+akshat170902@users.noreply.github.com>
2022-07-31 19:51:05 +0530

    Time: 7 ms (49.25%), Space: 8.1 MB (38.69%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,45 +1,31 @@
 class Solution {
 public:
-    void rec(vector<int>&nums,int idx,vector<vector<int>>&ans,vector<int>cur){
+    void subset(vector<int>& nums,vector<vector<int>>&ans,int idx,vector<int>cur) {
         
-        if(idx==nums.size()){
-            
+        int n=nums.size();
+        if(idx==n){
             ans.push_back(cur);
-            
             return;
         }
-        int take=0,notTake=0;
-        for(int i=idx;i<nums.size();i++){
-            if(i==idx){
-                
-                cur.push_back(nums[i]);
-                rec(nums,i+1,ans,cur);
-                cur.pop_back();
-                
+        for(int i=idx;i<n;i++){
+            if(i!=idx&&nums[i]==nums[i-1]){
+                continue;
             }
             else{
-                if(nums[i]==nums[i-1]){
-                    continue;
-                }
-                else{
-                    
-                    cur.push_back(nums[i]);
-                    rec(nums,i+1,ans,cur);
-                    cur.pop_back();
-                    
-                }
+                cur.push_back(nums[i]);
+                subset(nums,ans,i+1,cur);
+                cur.pop_back();
             }
             
         }
-        rec(nums,nums.size(),ans,cur);
-       
+        subset(nums,ans,n,cur);
         
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         vector<vector<int>>ans;
         vector<int>cur;
-        sort(begin(nums),end(nums));
-        rec(nums,0,ans,cur);
+        sort(nums.begin(),nums.end());
+        subset(nums,ans,0,cur);
         return ans;
     }
 };
