b3d617d07e4aeddda2046fd33c9db342b0595106
akshat170902 <95205174+akshat170902@users.noreply.github.com>
2022-08-01 22:32:54 +0530

    Time: 18 ms (32.12%), Space: 7.1 MB (95.55%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,49 +1,36 @@
 class Solution {
 public:
-    bool isSafe(vector< string>chess,int col,int row){
-    for(int i=0;i<row;i++){
-        if(chess[i][col]=='Q'){
-            return false;
+    bool isSafe(int &row,int &col,vector<string>&cur){
+        for(int i=row;i>=0;i--){
+            if(cur[i][col]=='Q')return false;
         }
-    }
-    for(int i=row-1,j=col-1;i>=0&&j>=0;i--,j--){
-        if(chess[i][j]=='Q'){
-            return false;
+        for(int i=col,j=row;i<cur.size()&&j>=0;i++,j--){
+            if(cur[j][i]=='Q')return false;
         }
-    }
-     for(int i=row-1,j=col+1;i>=0&&j<chess[0].size();i--,j++){
-        if(chess[i][j]=='Q'){
-            return false;
+        for(int i=col,j=row;i>=0&&j>=0;i--,j--){
+            if(cur[j][i]=='Q')return false;
         }
+        return true;
     }
-    return true;
-}
-    
-void printNQueens(vector<string>chess,vector<vector<string>>&s , int row) {
-    if(chess.size()==0)return;
-  if(row==chess.size()){
-    s.push_back(chess);
-      return;
-  }
-    
-  for(int i=0;i<chess[0].size();i++){
-      if(isSafe(chess,i,row)){
-          chess[row][i]='Q';
-          printNQueens(chess,s ,row+1);
-          chess[row][i]='.';
-      }
-  }
-}
-
-vector<vector<string>> solveNQueens(int n) {
-        string st="";
-        vector<vector<string>>v ;
+    void solve(int idx,vector<vector<string>>&ans,vector<string>&cur,int n){
+        if(idx==n){
+            ans.push_back(cur);
+            return;
+        }
         for(int i=0;i<n;i++){
-            st+='.';
+            if(isSafe(idx,i,cur)){
+                cur[idx][i]='Q';
+                solve(idx+1,ans,cur,n);
+                cur[idx][i]='.';
+            }
         }
-        vector<string>s(n,st);
-    
-        printNQueens(s,v ,0);
-        return v;
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>>ans;
+        string s="";
+        for(int i=0;i<n;i++)s+='.';
+        vector<string>cur(n,s);
+        solve(0,ans,cur,n);
+        return ans;
     }
 };
