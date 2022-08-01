07622b4a86e62bb2061f833ec7732f317054d63d
akshat170902 <95205174+akshat170902@users.noreply.github.com>
2022-08-01 17:53:51 +0530

    Time: 392 ms (6.37%), Space: 85.4 MB (10.46%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-    void rec(int idx,int tar,vector<int>&c,vector<int>&cur,vector<vector<int>>&ans){
+    void rec(vector<int>&candidates,int tar,vector<vector<int>>&ans,vector<int>cur,int idx){
+        int n=candidates.size();
+        if(tar<0||idx>=n){
+            return ;
+        }
         if(tar==0){
             ans.push_back(cur);
-            return;
+            return ;
         }
-        if(idx==c.size()||tar<0){
-            return;
-        }
-        rec(idx+1,tar,c,cur,ans);
-        cur.push_back(c[idx]);
-        rec(idx,tar-c[idx],c,cur,ans);
+        cur.push_back(candidates[idx]);
+        rec(candidates,tar-candidates[idx],ans,cur,idx);
         cur.pop_back();
-        
+        rec(candidates,tar,ans,cur,idx+1);
     }
-    vector<vector<int>> combinationSum(vector<int>& c, int tar) {
-        vector<int>cur;
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>>ans;
-        sort(c.begin(),c.end());
-        rec(0,tar,c,cur,ans);
+        vector<int>cur;
+        rec(candidates,target,ans,cur,0);
         return ans;
     }
 };
