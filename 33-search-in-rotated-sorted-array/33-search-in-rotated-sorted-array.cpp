1bb343c6c0364d242d0e03154216d79701b75983
akshat170902 <95205174+akshat170902@users.noreply.github.com>
2022-08-02 20:43:15 +0530

    Time: 13 ms (10.20%), Space: 11.2 MB (29.09%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,42 +1,51 @@
 class Solution {
 public:
-    int bin_search(vector<int>&nums,int lo,int hi,int tar){
-        while(lo<=hi){
-            int mid=(lo+hi)/2;
-            if(nums[mid]==tar)return mid;
-            else if(nums[mid]<tar)lo=mid+1;
-            else hi=mid-1;
+    int findMin(vector<int>& nums) {
+        int n=nums.size(),low=0,high=n-1;
+        int idx=-1;
+        if(nums[low]<=nums[high]){
+            return low;
+        }
+        while(low<high){
+            int mid=low+(high-low)/2;
+            if(mid<n&&nums[mid]>nums[mid+1]){
+                idx=mid+1;
+                break;
+            }
+            if(mid>0&&nums[mid]<nums[mid-1]){
+                idx=mid;
+                break;
+            }
+            if(nums[low]<nums[mid]){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return idx;
+    }
+    int bin(vector<int>&nums,int tar,int low,int high){
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==tar){
+                return mid;
+            }
+            else if(nums[mid]<tar){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
         }
         return -1;
     }
-    int search(vector<int>& nums, int target) {
-        int n=nums.size();
-        
-        int lo=0,hi=nums.size()-1;
-        if(nums[lo]<=nums[hi]){
-            return bin_search(nums,lo,hi,target);
+    int search(vector<int>& nums, int tar) {
+        int pvt=findMin(nums);
+        if(tar<nums[0]){
+            return bin(nums,tar,pvt,nums.size()-1);
+            
         }
-        int pvt=-1;
-        while(lo<=hi){
-            int mid=lo+(hi-lo)/2;
-            if(nums[mid]>nums[mid+1]){
-                pvt=mid+1;
-                break;
-            }
-            if(nums[mid]<nums[mid-1]){
-                pvt=mid;
-                break;
-            }
-            if(nums[mid]>nums[lo]){
-                lo=mid+1;
-            }
-            else{
-                hi=mid-1;
-            }
-        }
-        // cout<<pvt<<endl;
-        int l1=bin_search(nums,0,pvt-1,target);
-        int l2=bin_search(nums,pvt,n-1,target);
-        return (l1==-1)==(l2==-1)?-1:l1==-1?l2:l1;
+        return bin(nums,tar,0,(pvt-1+nums.size())%nums.size());
     }
 };
