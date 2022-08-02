e4bdc863fc1a3fd2ce9d4cc0a708c2fac73312fb
akshat170902 <95205174+akshat170902@users.noreply.github.com>
2022-08-02 20:10:08 +0530

    Time: 0 ms (100.00%), Space: 10.3 MB (23.27%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,35 +1,28 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        int lo=0,hi=nums.size()-1;
-        int pvt=-1;
-        if(hi==0)return nums[hi];
-        if(nums[lo]<nums[hi])return nums[lo];
-        while(lo<=hi){
-            int mid=lo+(hi-lo)/2;
-            if(mid==0){
-                if(nums[0]>nums[1]){
-                    pvt=1;
-                    break;
-                }
-            }
-            if(nums[mid]<nums[mid+1]&&nums[mid]>nums[mid-1]){
-                if(nums[mid]>nums[lo]){
-                    lo=mid+1;
-                }
-                else{
-                    hi=mid-1;
-                }
-            }
-            else if(nums[mid]>nums[mid+1]&&nums[mid]>nums[mid-1]){
-                pvt=mid+1;
+        int n=nums.size(),low=0,high=n-1;
+        int idx=-1;
+        if(nums[low]<=nums[high]){
+            return nums[low];
+        }
+        while(low<high){
+            int mid=low+(high-low)/2;
+            if(mid<n&&nums[mid]>nums[mid+1]){
+                idx=mid+1;
                 break;
+            }
+            if(mid>0&&nums[mid]<nums[mid-1]){
+                idx=mid;
+                break;
+            }
+            if(nums[low]<nums[mid]){
+                low=mid+1;
             }
             else{
-                pvt=mid;
-                break;
+                high=mid-1;
             }
         }
-        return nums[pvt];
+        return nums[idx];
     }
 };
