d4d8ec149a6b71d5fec4ab72787d596687e3ee0e
akshat170902 <95205174+akshat170902@users.noreply.github.com>
2022-08-23 11:50:34 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (61.16%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -1,49 +1,28 @@
 class Solution {
 public:
-    void PiFunction(vector<int>&lps,string&pat){
-int M=pat.size();
-    // length of the previous longest prefix suffix
-    int len = 0;
- 
-    lps[0] = 0; // lps[0] is always 0
- 
-    // the loop calculates lps[i] for i = 1 to M-1
-    int i = 1;
-    while (i < M) {
-        if (pat[i] == pat[len]) {
-            len++;
-            lps[i] = len;
-            i++;
-        }
-        else // (pat[i] != pat[len])
-        {
-            // This is tricky. Consider the example.
-            // AAACAAAA and i = 7. The idea is similar
-            // to search step.
-            if (len != 0) {
-                len = lps[len - 1];
- 
-                // Also, note that we do not increment
-                // i here
+    void PiFunction(vector<int>&arr,string&s){
+        int n=s.size();
+        for(int i=1;i<n;i++){
+            int j=arr[i-1];
+            while(j>0&&s[i]!=s[j]){
+                j=arr[j-1];
+                
             }
-            else // if (len == 0)
-            {
-                lps[i] = 0;
-                i++;
+            if(s[i]==s[j]){
+                j++;
             }
+            arr[i]=j;
         }
     }
-}
-    
     int knuthMoris(string &t,string&s){
         int n=s.size(),m=t.size();
         vector<int>arr(n);
         PiFunction(arr,s);
         int j=-1;
-        for(int i=0;i<n;i++){
-            cout<<arr[i]<<" ";
-        }
-        cout<<endl;
+        // for(int i=0;i<n;i++){
+        //     cout<<arr[i]<<" ";
+        // }
+        // cout<<endl;
         for(int i=0;i<m;){
             // cout<<i<<" "<<j<<endl;
             while(j+1<n&&s[j+1]==t[i]){
