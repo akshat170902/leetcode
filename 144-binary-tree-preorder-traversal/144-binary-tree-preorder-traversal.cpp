b5ebaf2d586f3058b53f72c4b9204ee5049b5d45
akshat170902 <95205174+akshat170902@users.noreply.github.com>
2022-08-24 12:56:56 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (99.44%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.cpp
@@ -11,20 +11,32 @@
  */
 class Solution {
 public:
+    TreeNode* rightMostFinder(TreeNode*cur,TreeNode*root){
+        while(cur->right&&cur->right!=root){
+            cur=cur->right;
+        }
+        return cur;
+    }
     vector<int> preorderTraversal(TreeNode* root) {
+        TreeNode*cur=root;
         vector<int>ans;
-        stack<TreeNode*>s;
-        if(root)
-            s.push(root);
-        while(!s.empty()){
-            TreeNode*cur=s.top();
-            ans.push_back(cur->val);
-            s.pop();
-            if(cur->right){
-                s.push(cur->right);
+        while(cur){
+            TreeNode*leftNode=cur->left;
+            if(leftNode){
+                TreeNode*RightMost=rightMostFinder(leftNode,cur);
+                if(RightMost->right){
+                    RightMost->right=nullptr;
+                    cur=cur->right;
+                }
+                else{
+                    RightMost->right=cur;
+                    ans.push_back(cur->val);
+                    cur=cur->left;
+                }
             }
-            if(cur->left){
-                s.push(cur->left);
+            else{
+                ans.push_back(cur->val);
+                cur=cur->right;
             }
         }
         return ans;
