e98876c507ffea133254a9211b810412cf672185
akshat170902 <95205174+akshat170902@users.noreply.github.com>
2022-08-25 19:45:56 +0530

    Time: 23 ms (82.90%), Space: 17.2 MB (39.08%) - LeetHub

---
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.cpp
@@ -0,0 +1,41 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        queue<Node*>q;
+        if(root)
+            q.push(root);
+        while(!q.empty()){
+            int n=q.size();
+            for(int i=0;i<n;i++){
+                Node*p=q.front();
+                q.pop();
+                if(i!=n-1){
+                    p->next=q.front();
+                }
+                if(p->left)
+                    q.push(p->left);
+                if(p->right)
+                    q.push(p->right);
+            }
+        }
+        return root;
+    }
+};
