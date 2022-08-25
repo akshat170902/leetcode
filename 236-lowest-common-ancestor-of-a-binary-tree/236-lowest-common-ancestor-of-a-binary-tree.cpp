f81acab07adbe1620b504a24006ec9b970cf847b
akshat170902 <95205174+akshat170902@users.noreply.github.com>
2022-08-25 07:42:02 +0530

    Time: 22 ms (65.66%), Space: 14.2 MB (57.31%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -10,13 +10,16 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(root==nullptr)return nullptr;
-        if(root==p||root==q)return root;
-        TreeNode*l=lowestCommonAncestor(root->left,p,q);
-        TreeNode*r=lowestCommonAncestor(root->right,p,q);
-        if(l&&r)return root;
-        if(l)return l;
-        return r;
-            
+        if(!root)
+            return nullptr;
+        if(root==p||root==q)
+            return root;
+        TreeNode*leftLCA=lowestCommonAncestor(root->left,p,q);
+        TreeNode*rightLCA=lowestCommonAncestor(root->right,p,q);
+        if(leftLCA&&rightLCA)
+            return root;
+        if(leftLCA)
+            return leftLCA;
+        return rightLCA;
     }
 };
