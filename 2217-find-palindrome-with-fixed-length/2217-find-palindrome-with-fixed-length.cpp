2dfc65bd4da49498ce19460badb35771acdbf002
akshat170902 <95205174+akshat170902@users.noreply.github.com>
2022-09-09 19:06:58 +0530

    Time: 396 ms (18.04%), Space: 54.1 MB (12.11%) - LeetHub

--- 2217-find-palindrome-with-fixed-length/2217-find-palindrome-with-fixed-length.cpp
+++ 2217-find-palindrome-with-fixed-length/2217-find-palindrome-with-fixed-length.cpp
@@ -10,30 +10,7 @@ public:
                 ans.push_back(-1);
             }
             else{
-                if(intLength%2){
-                    int cur=queries[i]-1;
-                    
-                    string s="";
-                    for(int j=0;j<intLength;j++){
-                        s+='0';
-                    }
-                    
-                    for(int j=(intLength)/2;j>=0;j--){
-                        int data=cur%10;
-                        cur=cur/10;
-                        if(j==0){
-                            s[j]=1+data+'0';
-                            s[intLength-1-j]=1+data+'0';
-                        }
-                        else{
-                            s[j]=data+'0';
-                            s[intLength-1-j]=data+'0';
-                        }
-                    }
-                    // cout<<s<<endl;
-                    ans.push_back(stoll(s));
-                }
-                else{
+                // if(intLength%2){
                     int cur=queries[i]-1;
                     
                     string s="";
@@ -55,7 +32,30 @@ public:
                     }
                     // cout<<s<<endl;
                     ans.push_back(stoll(s));
-                }
+//                 }
+//                 else{
+//                     int cur=queries[i]-1;
+                    
+//                     string s="";
+//                     for(int j=0;j<intLength;j++){
+//                         s+='0';
+//                     }
+                    
+//                     for(int j=(intLength-1)/2;j>=0;j--){
+//                         int data=cur%10;
+//                         cur=cur/10;
+//                         if(j==0){
+//                             s[j]=1+data+'0';
+//                             s[intLength-1-j]=1+data+'0';
+//                         }
+//                         else{
+//                             s[j]=data+'0';
+//                             s[intLength-1-j]=data+'0';
+//                         }
+//                     }
+//                     // cout<<s<<endl;
+//                     ans.push_back(stoll(s));
+//                 }
                 
             }
         }
