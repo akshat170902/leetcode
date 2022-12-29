8f139f5e18d376f11f8e67da45d267e6f0335bcb
akshat170902 <95205174+akshat170902@users.noreply.github.com>
2022-12-29 12:16:39 +0530

    Time: 683 ms (83.07%), Space: 130.8 MB (43.91%) - LeetHub

---
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<int> getOrder(vector<vector<int>>& tasks) {
+        vector<int>ans;
+        // vector<vector<int>>task;
+        for(int i=0;i<tasks.size();i++){
+            
+            tasks[i].push_back(i);
+            // cout<<tasks[i][0]<<" "<<tasks[i][1]<<" "<<tasks[i][2]<<endl;
+        }
+        sort(tasks.begin(),tasks.end());
+//         for(int i=0;i<tasks.size();i++){
+            
+//             // tasks[i].push_back(i);
+//             cout<<tasks[i][0]<<" "<<tasks[i][1]<<" "<<tasks[i][2]<<endl;
+//         }
+        vector<int>flag(tasks.size());
+        long long start=1,i=0;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        while(i<tasks.size()){
+        
+            for(;i<tasks.size()&&tasks[i][0]<=start;i++){
+                pq.push({tasks[i][1],tasks[i][2]});
+            }
+            if(pq.empty()){
+                ans.push_back(tasks[i][2]);
+                start=tasks[i][1]+tasks[i][0];
+                i++;
+            }
+            else{
+                ans.push_back(pq.top().second);
+                start+=pq.top().first;
+                pq.pop();
+
+            }
+            // cout<<pq.size();
+        }
+        while(pq.size()){
+            ans.push_back(pq.top().second);
+                start+=pq.top().first;
+                pq.pop();
+        }
+        return ans;
+    }
+};
