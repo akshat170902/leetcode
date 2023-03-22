9a9ffb4dd9e9494ebb90a6ec8159ad8e5c46741d
akshat170902 <95205174+akshat170902@users.noreply.github.com>
2023-03-22 08:25:09 +0530

    Time: 459 ms (81.85%), Space: 128.8 MB (60.62%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -1,28 +1,35 @@
 class Solution {
 public:
-    void dfs(int node,vector<vector<pair<int,int>>>&graph,vector<int>&vis,vector<int>&ans){
+    void dfs(int node,vector<vector<pair<int,int>>>&graph,vector<int>&vis){
         vis[node]=0;
         for(auto&child:graph[node]){
             if(vis[child.first]){
-                ans.push_back(child.first);
-                dfs(child.first,graph,vis,ans);
+                
+                dfs(child.first,graph,vis);
             }
         }
     }
     int minScore(int n, vector<vector<int>>& roads) {
-        vector<int>vis(n,1);
+	
         vector<vector<pair<int,int>>>graph(n);
+		//First we will create a graph with the given edges and weights
+		//by making graph a vector of vector of pair
+		
         for(auto&road:roads){
             
             graph[road[0]-1].push_back({road[1]-1,road[2]});
             graph[road[1]-1].push_back({road[0]-1,road[2]});
         }
-        
-        vector<int>ans;
-        dfs(0,graph,vis,ans);
+		//for the dfs we require a visited array so we created a vis vector
+        vector<int>vis(n,1);
+		
+        dfs(0,graph,vis);
         int res=1e9;
-        for(int i=0;i<ans.size();i++){
-            for(auto&child:graph[ans[i]]){
+        for(int i=0;i<vis.size();i++){
+            if(vis[i]){
+                continue;
+            }
+            for(auto&child:graph[i]){
                 res=min(res,child.second);
             }
         }
