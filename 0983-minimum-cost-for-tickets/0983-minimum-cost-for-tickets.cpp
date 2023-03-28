40dfd0d45e1c31c955e25fad87205c1ac05912b2
akshat170902 <95205174+akshat170902@users.noreply.github.com>
2023-03-28 07:55:55 +0530

    Time: 0 ms (100.00%), Space: 9.7 MB (30.82%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
     int  helper(int idx,int val,vector<int>&days,vector<int>&costs,vector<int>&dp){
-       
-        while(idx<days.size()&&days[idx]<val){
-            idx++;
-        }
-         if(idx>=days.size()||val>365){
+        if(idx>=days.size()){
             return 0;
         }
-        if(dp[idx]==0){
-            dp[idx]=min(helper(idx+1,days[idx]+1,days,costs,dp)+costs[0],min(helper(idx+1,days[idx]+7,days,costs,dp)+costs[1],helper(idx+1,days[idx]+30,days,costs,dp)+costs[2]));
+        if(dp[idx]!=-1){
+            return dp[idx];
         }
-        return dp[idx];
+        int pass1=upper_bound(days.begin(),days.end(),days[idx]+1-1)-days.begin();
+        int c1=helper(pass1,val+1,days,costs,dp)+costs[0];
+        int pass7=upper_bound(days.begin(),days.end(),days[idx]+7-1)-days.begin();
+        int c7=helper(pass7,val+7,days,costs,dp)+costs[1];
+        int pass3=upper_bound(days.begin(),days.end(),days[idx]+30-1)-days.begin();
+        int c3=helper(pass3,val+30,days,costs,dp)+costs[2];
+        
+        return dp[idx]=min(min(c1,c7),c3);
     }
     int mincostTickets(vector<int>& days, vector<int>& costs) {
-        vector<int>dp(days.size()+1);
+        vector<int>dp(days.size()+1,-1);
         return helper(0,0,days,costs,dp);
       
     }
