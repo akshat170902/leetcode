0e9dd3397d55c0ec82b314e60bb3590629930e4a
akshat170902 <95205174+akshat170902@users.noreply.github.com>
2023-09-13 12:06:49 +0530

    Time: 14 ms (83.40%), Space: 18.3 MB (11.66%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int candy(vector<int>& rating) {
+        
+        int n=rating.size();
+        vector<int>val(n,1),value(n,1);
+        
+        int left=n-2,right=1;
+        // cout<<left<<endl;
+        while(left>=0){
+            if(rating[left]>rating[left+1]){
+                val[left]=val[left+1]+1;
+            }
+            else{
+                val[left]=1;
+            }
+            left--;
+        }
+        while(right<n){
+            if(rating[right]>rating[right-1]){
+                value[right]=value[right-1]+1;
+            }
+            else{
+                value[right]=1;
+            }
+            right++;
+        }
+        int ans=0;
+        for(int i=0;i<n;i++){
+            ans+=max(value[i],val[i]);
+        }
+        return ans;
+    }
+};
