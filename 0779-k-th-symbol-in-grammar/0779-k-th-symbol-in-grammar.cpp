77c8d474fa4890c8f376d2dfe81e0d1509bae088
akshat170902 <95205174+akshat170902@users.noreply.github.com>
2023-10-26 09:26:44 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (55.07%) - LeetHub

---
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int depthFirstSearch(int n, int k, int rootVal) {
+        if (n == 1) {
+            return rootVal;
+        }
+
+        int totalNodes = pow(2, n - 1);
+
+        // Target node will be present in the right half sub-tree of the current root node.
+        if (k > (totalNodes / 2)) {
+            int nextRootVal = (rootVal == 0) ? 1 : 0;
+            return depthFirstSearch(n - 1, k - (totalNodes / 2), nextRootVal);
+        }
+        // Otherwise, the target node is in the left sub-tree of the current root node.
+        else {
+            int nextRootVal = (rootVal == 0) ? 0 : 1;
+            return depthFirstSearch(n - 1, k, nextRootVal);
+        }
+    }
+
+    int kthGrammar(int n, int k) {
+        return depthFirstSearch(n, k, 0);
+    }
+};
