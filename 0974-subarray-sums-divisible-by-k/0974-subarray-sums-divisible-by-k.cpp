5f9a16ba5028b6e51db099fbc697f9d80cee2b2d
akshat170902 <95205174+akshat170902@users.noreply.github.com>
2024-06-09 21:42:31 +0530

    Time: 36 ms (74.52%), Space: 35.2 MB (18.10%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
@@ -1,31 +1,15 @@
 class Solution {
 public:
-    int negMod(int n,int mod){
-        
-        if(n<0){
-            n=abs(n)%mod;
-            return (-n+mod)%mod;
-        }
-        
-        return n%mod;
-    }
     int subarraysDivByK(vector<int>& nums, int k) {
-        int n=nums.size();
-        
-        int cur=0,ans=0;
+        int result=0,prev=0;
         unordered_map<int,int>m;
         m[0]++;
-        
-        for(int num:nums){
-            
-            cur=(cur+negMod(num,k))%k;
-            
-            if(m.count(cur)){
-                ans+=m[cur];
-                
-            }
-            m[cur]++;
+        for(auto&num:nums){
+            int val=((num+prev)%k+k)%k;
+            prev=val;
+            result+=m[val];
+            m[val]++;
         }
-        return ans;
+        return result;
     }
 };
