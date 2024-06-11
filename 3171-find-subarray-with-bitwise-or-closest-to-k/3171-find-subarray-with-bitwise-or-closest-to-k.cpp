ebdd175deebf2c42e705796253689759f37c661e
akshat170902 <95205174+akshat170902@users.noreply.github.com>
2024-06-11 23:28:27 +0530

    Time: 702 ms (52.63%), Space: 344.9 MB (23.79%) - LeetHub

---
+++ 3171-find-subarray-with-bitwise-or-closest-to-k/3171-find-subarray-with-bitwise-or-closest-to-k.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minimumDifference(vector<int>& nums, int k) {
+        set<int> prev;
+        int n = nums.size();
+        int minV = INT_MAX;
+        for (int i = 0; i < n; i++){
+            set<int> next;
+            for (int v : prev)
+                next.insert(v | nums[i]);
+            next.insert(nums[i]);
+
+            for (int v : next)
+                minV = min(minV, abs(k - v));
+
+            prev = next;
+        }
+        return minV;
+    }
+};
