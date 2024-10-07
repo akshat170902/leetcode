7a525c5af8b866bcff909d9dddcfc5475273e928
Akshat <95205174+akshat170902@users.noreply.github.com>
2024-10-07 22:59:01 +0530

    Time: 24 ms (5.05%), Space: 11.7 MB (31.24%) - LeetHub

---
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minLength(string s) {
+        stack<char>st;
+        int i=0,n=s.size();
+        st.push(s[i]);
+        i++;
+        while(i<n){
+            
+            cout<<st.size()<<endl;
+            if((st.size()>0)&&((s[i]=='B'&&st.top()=='A')||(s[i]=='D'&&st.top()=='C'))){
+                
+                st.pop();
+                // cout<<s[i]<<st.size()<<endl;
+            }
+            else{
+                // cout<<s[i]<<endl;
+                st.push(s[i]);
+            }
+           i++;
+        }
+        cout<<st.size();
+        return st.size();
+    }
+};
